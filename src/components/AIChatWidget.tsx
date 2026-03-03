74b81e16dc8518b05ec2ff856f6d1702112e0f98
ZMensRain <zmenrain@protonmail.com>
2026-03-03 17:15:17 +0200

    - added a component for the message box - added shape to the chat widget - added an icon library

--- src/components/AIChatWidget.tsx
+++ src/components/AIChatWidget.tsx
@@ -1,7 +1,37 @@
-// type Props = {
-//   children: React.ReactNode;
-// };
+type Props = {
+  open?: boolean;
+};
 
-export default function AIChatWidget() {
-  return <div className=""></div>;
+import { BotMessageSquare } from "lucide-react";
+import { useState } from "react";
+import ChatBox from "./ChatBox";
+
+export default function AIChatWidget(props: Props) {
+  const [dialogOpen, setDialogOpen] = useState(props.open ?? false);
+
+  function handleClick() {
+    setDialogOpen(!dialogOpen);
+  }
+
+  return (
+    <>
+      <button
+        className="rounded-xl bg-[#f15519] p-4 fixed bottom-4 right-4 cursor-pointer hover:scale-110 transition"
+        onClick={handleClick}
+        aria-label="Toggle AI chat"
+        aria-toggle="true"
+        aria-expanded={dialogOpen}
+      >
+        <BotMessageSquare scale={3} />
+      </button>
+      {dialogOpen && (
+        <div className="fixed right-4 bottom-20 max-w-100 w-full min-h-150 bg-gray-800 p-4 rounded-xl flex flex-col shadow-2xl">
+          <h2 className="text-2xl font-bold">Aheeva AI Chat</h2>
+
+          <div className="flex-1"></div>
+          <ChatBox onMessageSend={(message) => console.log(message)} />
+        </div>
+      )}
+    </>
+  );
 }
---
+++ src/components/ChatBox.tsx
@@ -0,0 +1,56 @@
+import { SendIcon } from "lucide-react";
+import React from "react";
+
+type Props = {
+  onMessageSend?: (message: string) => void;
+};
+
+export default function ChatBox({ onMessageSend }: Props) {
+  // input reference
+
+  const handleSubmit: React.SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const formDate = new FormData(e.target as HTMLFormElement);
+    const message = formDate.get("message") as string;
+
+    if (!message) {
+      alert("Please enter a message");
+      return;
+    }
+    (e.target as HTMLFormElement).reset();
+    onMessageSend?.(message);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="group text-left flex flex-col gap-1"
+    >
+      <label
+        className="text-white text-left mb-1 group-focus-within:text-[#f15519] group-focus-within:font-semibold transition"
+        htmlFor="aheeva-chat-input"
+      >
+        Message
+      </label>
+
+      <div className="flex-1 border-2 border-gray-700 rounded-xl flex flex-row items-center pr-2 group-focus-within:border-[#f15519]  transition">
+        <textarea
+          id="aheeva-chat-input"
+          className=" text-white p-4 border-none outline-0 rounded-xl w-full resize-none"
+          placeholder="What does the company do?"
+          name="message"
+          required
+        ></textarea>
+
+        <button
+          className="bg-[#f15519] p-2 rounded-xl hover:opacity-90 transition cursor-pointer"
+          type="submit"
+          aria-label="Send message"
+        >
+          <SendIcon />
+        </button>
+      </div>
+    </form>
+  );
+}
