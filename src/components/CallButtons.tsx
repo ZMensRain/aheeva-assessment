ae27634fd8f08260d76c9c3e1915977e56ad8fd8
ZMensRain <zmenrain@protonmail.com>
2026-03-04 11:49:58 +0200

    improved button look

--- src/components/CallButtons.tsx
+++ src/components/CallButtons.tsx
@@ -16,9 +16,18 @@ export default function CallButtons({
   return (
     <div className="flex flex-row gap-2">
       <button
+        className={`flex-2  rounded-2xl  transition cursor-pointer flex flex-row justify-center items-center gap-2 hover:opacity-90 ${
+          isStartCall ? "bg-black" : "bg-red-600 "
+        }`}
+        onClick={() => onCallButton(isStartCall)}
+      >
+        <PhoneIcon />
+        {!isStartCall ? "End Call" : "Start Call"}
+      </button>
+      <button
         className={`${
-          muted ? "bg-accent hover:bg-amber-600" : "bg-red-500 hover:bg-red-600"
-        } rounded-2xl flex flex-row justify-center items-center p-2  gap-2 flex-1 cursor-pointer`}
+          muted ? "bg-accent " : "bg-red-600 "
+        } rounded-2xl flex flex-row justify-center items-center p-2  gap-2 flex-1 cursor-pointer hover:opacity-90`}
         onClick={onMuteToggle}
       >
         {muted ? (
@@ -30,17 +39,6 @@ export default function CallButtons({
             <MicIcon /> Mute
           </>
         )}
-      </button>
-      <button
-        className={`flex-2  rounded-2xl  transition cursor-pointer flex flex-row justify-center items-center gap-2 ${
-          isStartCall
-            ? "bg-accent hover:bg-amber-600"
-            : "bg-red-500 p-2 hover:bg-red-600"
-        }`}
-        onClick={() => onCallButton(isStartCall)}
-      >
-        <PhoneIcon />
-        {!isStartCall ? "End Call" : "Start Call"}
       </button>
     </div>
   );
